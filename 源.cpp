c0a90c8c8745c61eef29c60fcd51b3bf302b6b33
Horace529 <>
2026-03-10 10:56:45 +0800

    修改了项目配置方式，现在只要电脑有VK的SDK和环境变量就能用了

--- 源.cpp
+++ 源.cpp
@@ -1,7 +1,6 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
@@ -17,7 +16,7 @@ int main() {
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    std::cout << extensionCount << " extensions supported" << std::endl;
+    std::cout << extensionCount << " extensions supported\n";
 
     glm::mat4 matrix;
     glm::vec4 vec;
